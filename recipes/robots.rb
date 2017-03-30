66c5802b4d063777c864be4997b1be8e86b5a3f6
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-03-30 15:36:22 -0700

    Ensure directory structure is in place before creating robots.txt

--- recipes/robots.rb
+++ recipes/robots.rb
@@ -3,6 +3,21 @@
 # Recipe:: robots
 #
 
+# Ensure directory structure for robots.txt
+robot_dirs = %w(
+    /var/www/shared
+    /var/www/shared/pub
+)
+
+robot_dirs.each do |dir|
+    directory dir do
+        owner     node['magento']['users']['cli']['name']
+        group     node['magento']['groups']['www-data']['name']
+        mode      0755
+        recursive true
+    end
+end
+
 # Create 'robots.txt' from template
 template 'Creating robots.txt' do
     path    '/var/www/shared/pub/robots.txt'
