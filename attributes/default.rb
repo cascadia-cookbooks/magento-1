5025588902908e9b2ce1b42bdecd008ea69e5491
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-03-30 14:15:59 -0700

    Add defaults for Magento users and groups

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,18 @@ default['magento']['tools']['composer']['source'] = 'composer.phar'
 default['magento']['tools']['composer']['owner']  = 'mage-cli'
 default['magento']['tools']['composer']['group']  = 'cli'
 default['magento']['tools']['composer']['mode']   = '0755'
-default['magento']['tools']['magenrun']['path']   = '/user/local/bin/n98-magerun2'
-default['magento']['tools']['magenrun']['source'] = 'n98-magerun2.phar'
-default['magento']['tools']['magenrun']['owner']  = 'mage-cli'
-default['magento']['tools']['magenrun']['group']  = 'cli'
-default['magento']['tools']['magenrun']['mode']   = '0755'
+default['magento']['tools']['magerun']['path']    = '/user/local/bin/n98-magerun2'
+default['magento']['tools']['magerun']['source']  = 'n98-magerun2.phar'
+default['magento']['tools']['magerun']['owner']   = 'mage-cli'
+default['magento']['tools']['magerun']['group']   = 'cli'
+default['magento']['tools']['magerun']['mode']    = '0755'
+
+default['magento']['users']['cli']['name']  = 'mage-cli'
+default['magento']['users']['cli']['group'] = 'cli'
+default['magento']['users']['www']['name']  = 'www-data'
+default['magento']['users']['www']['group'] = 'www-data'
+
+default['magento']['groups']['cli']['name'] = 'cli'
+default['magento']['groups']['cli']['members'] = ['mage-cli']
+default['magento']['groups']['www-data']['name'] = 'www-data'
+default['magento']['groups']['www-data']['members'] = ['www-data', 'mage-cli']
