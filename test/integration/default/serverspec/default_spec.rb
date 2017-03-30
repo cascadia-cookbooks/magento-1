a3f2021b0d91b408edc3e37f2d57fd64f90308e7
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-03-30 11:07:51 -0700

    Tests for Magento CLI tools

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,3 +2,19 @@ require 'spec_helper'
 
 describe 'magento::default' do
 end
+
+describe 'magento::cli-tools' do
+    tools = %w(
+        composer
+        n98-magerun2
+    )
+    
+    tools.each do |tool|
+        describe file("/usr/local/bin/#{tool}") do
+            it { should exist }
+            it { should be_owned_by 'mage-cli' }
+            it { should be_grouped_into 'cli' }
+            it { should be_mode '0755' }
+        end
+    end
+end
