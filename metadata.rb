5b877981f79582a5eb2cbc1a0d08c5dc8f2109e6
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-03-30 10:18:55 -0700

    Basic metadata

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name                'cop_magento'
+description         'Prepares the environment for Magento 2'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+license             'MIT'
+maintainer          'Copious, Inc.'
+maintainer_email    'engineering@copiousinc.com'
+version             '0.0.0'
+source_url          'https://github.com/copious-cookbooks/magento'
+issues_url          'https://github.com/copious-cookbooks/magento/issues'
+
+supports 'ubuntu', '>=14.04'
+supports 'debian', '>= 7'
+supports 'rhel', '>= 6'
+supports 'centos', '>= 6'
+
+# TODO: depends
