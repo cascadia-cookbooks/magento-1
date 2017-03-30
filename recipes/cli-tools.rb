6701370571ecc0096ced1c1fd0c72c6718996589
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-03-30 10:52:07 -0700

    CLI tools recipe

---
+++ recipes/cli-tools.rb
@@ -0,0 +1,15 @@
+
+#
+# Cookbook Name:: magento
+# Recipe:: cli-tools
+#
+
+node['magento']['tools'].each do |tool_node, tool_attr|
+    cookbook_file tool_attr['path'] do
+        source  tool_attr['source']
+        owner   tool_attr['owner']
+        group   tool_attr['group']
+        mode    tool_attr['mode']
+        action  :create
+    end
+end
