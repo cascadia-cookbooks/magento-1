f516ce9f1fd480b46e51929ff0f7e7c6fdc013d8
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-05 12:19:33 -0700

    Update paths and ownership

--- recipes/composer-prep.rb
+++ recipes/composer-prep.rb
@@ -3,15 +3,25 @@
 # Recipe:: composer-prep
 #
 
-cli_user                = node['magento']['users']['cli']['name']
-cli_group               = node['magento']['groups']['cli']['name']
+cli_user    = node['magento']['users']['cli']['name']
+cli_group   = node['magento']['users']['cli']['group']
 
-composer_home           = node['magento']['directories']['composer_home']['path']
-magento_composer_home   = node['magento']['directories']['magento_composer_home']['path']
-magento_path            = node['magento']['directories']['magento']['path']
+www_user    = node['magento']['users']['www']['name']
+www_group   = node['magento']['users']['www']['group']
 
-template 'Installing composer auth.json' do
-    path    "#{composer_home}/auth.json"
+docroot     = "/var/www/#{node['magento']['domain']}"
+
+# Create directory for cli_user Composer auth.json
+directory "/home/#{cli_user}/.composer" do
+    owner   cli_user
+    group   cli_group
+    mode    0755
+    action  :create
+end
+
+# Generate Composer auth.json for CLI user
+template 'Installing CLI user Composer auth.json' do
+    path    "/home/#{cli_user}/.composer/auth.json"
     source  'composer/auth.json.erb'
     owner   cli_user
     group   cli_group
@@ -20,21 +30,23 @@ template 'Installing composer auth.json' do
     backup  false
 end
 
-template 'Installing magento composer auth.json' do
-    path    "#{magento_composer_home}/auth.json"
+# Generate Composer auth.json for Magento in shared directory
+template 'Installing Magento Composer auth.json' do
+    path    "#{docroot}/shared/composer/auth.json"
     source  'composer/auth.json.erb'
-    owner   cli_user
-    group   cli_group
+    owner   www_user
+    group   www_group
     mode    0644
     action  :create
     backup  false
 end
 
+# Generate Composer composer.json in shared directory
 template 'Creating composer.json' do
-    path    "#{magento_path}/composer.json"
+    path    "#{docroot}/shared/composer/composer.json"
     source  'composer/composer.json.erb'
-    owner   cli_user
-    group   cli_group
+    owner   www_user
+    group   www_group
     mode    0644
     action  :create
     backup  false
