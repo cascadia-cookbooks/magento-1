4008b5d6f50c344d958b661d83e389b2658e8979
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-05 12:18:38 -0700

    Create docroot directory structure

---
+++ recipes/dir-structure.rb
@@ -0,0 +1,60 @@
+#
+# Cookbook Name:: magento
+# Recipe:: dir-structure
+#
+
+# Creates directory structure for the /var/www/ docroot
+
+default_domain = node['magento']['domain']
+additional_domains = node['magento']['additional_domains']
+
+dirs = %w(
+    shared
+    shared/app
+    shared/app/etc
+    shared/composer
+    shared/pub
+    shared/pub/static
+    shared/pub/media
+    shared/var
+)
+
+# Create structure for default domain
+dirs.each do |dir|
+    directory "/var/www/#{default_domain}" do
+        owner     node['magento']['users']['www']['name']
+        group     node['magento']['users']['www']['group']
+        mode      0755
+        recursive true
+        action    :create
+    end
+
+    directory "/var/www/#{default_domain}/#{dir}" do
+        owner     node['magento']['users']['www']['name']
+        group     node['magento']['users']['www']['group']
+        mode      0755
+        recursive true
+        action    :create
+    end
+end
+
+# Create structure for any additional domains
+additional_domains.each do |domain|
+    directory "/var/www/#{domain}" do
+        owner     node['magento']['users']['www']['name']
+        group     node['magento']['users']['www']['group']
+        mode      0755
+        recursive true
+        action    :create
+    end
+
+    dirs.each do |dir|
+        directory "/var/www/#{domain}/#{dir}" do
+            owner     node['magento']['users']['www']['name']
+            group     node['magento']['users']['www']['group']
+            mode      0755
+            recursive true
+            action    :create
+        end
+    end
+end
