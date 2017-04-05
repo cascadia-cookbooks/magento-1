cccf8449368793b6c0fc847c679a6c9eeada9ea8
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-05 12:31:28 -0700

    Generate robots.txt for additional domains

--- recipes/robots.rb
+++ recipes/robots.rb
@@ -3,29 +3,30 @@
 # Recipe:: robots
 #
 
-# Ensure directory structure for robots.txt
-robot_dirs = %w(
-    /var/www/shared
-    /var/www/shared/pub
-)
-
-robot_dirs.each do |dir|
-    directory dir do
-        owner     node['magento']['users']['cli']['name']
-        group     node['magento']['groups']['www-data']['name']
-        mode      0755
-        recursive true
-    end
-end
+default_domain = node['magento']['domain']
+additional_domains = node['magento']['additional_domains']
 
 # Create 'robots.txt' from template
 template 'Creating robots.txt' do
-    path    '/var/www/shared/pub/robots.txt'
+    path    "/var/www/#{default_domain}/shared/pub/robots.txt"
     source  'robots/robots.txt.erb'
-    owner   node['magento']['users']['cli']['name']
-    group   node['magento']['groups']['www-data']['name']
+    owner   node['magento']['users']['www']['name']
+    group   node['magento']['users']['www']['group']
     mode    0644
     action  :create
     backup  false
     only_if {node['robots']}
+end
+
+additional_domains.each do |domain|
+    template 'Creating additional domain robots.txt' do
+        path    "/var/www/#{domain}/shared/pub/robots.txt"
+        source  'robots/robots.txt.erb'
+        owner   node['magento']['users']['www']['name']
+        group   node['magento']['users']['www']['group']
+        mode    0644
+        action  :create
+        backup  false
+        only_if {node['robots']}
+    end
 end
