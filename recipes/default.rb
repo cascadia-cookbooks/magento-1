65f555a6ba8f5f188eefe9ae28e185787cd2de79
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-10 10:13:08 -0700

    Adjust run list

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,8 +5,9 @@
 
 include_recipe 'cop_magento::data-bag'
 include_recipe 'cop_magento::users'
-include_recipe 'cop_magento::permissions'
 include_recipe 'cop_magento::cli-tools'
+include_recipe 'cop_magento::dir-structure'
+include_recipe 'cop_magento::permissions'
 include_recipe 'cop_magento::robots'
 include_recipe 'cop_magento::composer-prep'
 include_recipe 'cop_magento::magento-install'
