b906efc9d676af850f7b7dcdc20f40261cd1e489
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-10 10:16:34 -0700

    Change ownership

--- recipes/robots.rb
+++ recipes/robots.rb
@@ -6,12 +6,18 @@
 docroot            = node['magento']['docroot']
 additional_domains = node['magento']['additional_domains']
 
+cli_user           = node['magento']['users']['cli']['name']
+cli_group          = node['magento']['users']['cli']['group']
+
+www_user           = node['magento']['users']['www']['name']
+www_group          = node['magento']['users']['www']['group']
+
 # Create 'robots.txt' from template
 template 'Creating robots.txt' do
     path    "#{docroot}/shared/pub/robots.txt"
     source  'robots/robots.txt.erb'
-    owner   node['magento']['users']['www']['name']
-    group   node['magento']['users']['www']['group']
+    owner   cli_user
+    group   www_group
     mode    0644
     action  :create
     backup  false
@@ -22,8 +28,8 @@ additional_domains.each do |domain|
     template "Creating #{domain} robots.txt" do
         path    "/var/www/#{domain}/shared/pub/robots.txt"
         source  'robots/robots.txt.erb'
-        owner   node['magento']['users']['www']['name']
-        group   node['magento']['users']['www']['group']
+        owner   cli_user
+        group   www_group
         mode    0644
         action  :create
         backup  false
