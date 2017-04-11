15de8617ad1bb2cdb823160afc7770426944fda5
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-11 16:18:01 -0700

    Include Vagrant dir recipe for 'development' environment

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,9 @@ include_recipe 'cop_magento::data-bag'
 include_recipe 'cop_magento::users'
 include_recipe 'cop_magento::cli-tools'
 include_recipe 'cop_magento::dir-structure'
+if node.chef_environment == "development"
+    include_recipe 'cop_magento::dir-structure-vagrant'
+end
 include_recipe 'cop_magento::permissions'
 include_recipe 'cop_magento::robots'
 include_recipe 'cop_magento::composer-prep'
