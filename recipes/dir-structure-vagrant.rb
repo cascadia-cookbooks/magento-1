1a1d12c8a77d52ea0a98bb7765f06e84cc355e92
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-11 16:17:28 -0700

    Directory structure for Vagrant environment

---
+++ recipes/dir-structure-vagrant.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: magento
+# Recipe:: dir-structure-vagrant
+#
+
+# Create directory structure for Vagrant dev environment
+
+dirs = %w(
+    /vagrant
+    /vagrant/magento
+    /vagrant/magento/app
+    /vagrant/magento/app/etc
+    /vagrant/magento/vendor
+)
+
+dirs.each do |dir|
+    directory dir do
+        owner     'vagrant'
+        group     'vagrant'
+        mode      0775
+        recursive true
+        action    :create
+    end
+end
