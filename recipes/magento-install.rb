7f5ce5259bf722252ca12996a173e723958d9acd
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-11 13:23:48 -0700

    Remove NGINX vhost tasks

--- recipes/magento-install.rb
+++ recipes/magento-install.rb
@@ -169,14 +169,3 @@ execute 'Magento setup:upgrade' do
     user    cli_user
     group   www_group
 end
-
-php_pool = node['magento']['application']['php_fpm_pool']
-fpm_location = node['php']['sapi']['fpm']['conf']['pools'][php_pool]['listen'].tr('"', '')
-
-# Set Magento 2 vhost
-magento_vhost node['magento']['domain'] do
-    nginx_listen '0.0.0.0:80'
-    docroot      "#{docroot}/current/pub"
-    fpm_location "unix:#{fpm_location}"
-    action       :create
-end
