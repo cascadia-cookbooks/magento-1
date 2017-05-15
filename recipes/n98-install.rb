33480341e7d14f0ae060a5f9282ea67443ddd81f
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-15 16:12:23 -0700

    Fetch n98-magerun2

---
+++ recipes/n98-install.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: magento
+# Recipe:: n98-install
+#
+
+n98_version  = node['magento']['n98']['version']
+n98_checksum = node['magento']['n98']['checksum']
+n98_owner    = node['magento']['cli_user']['name']
+n98_group    = node['magento']['cli_user']['group'] 
+
+remote_file '/usr/local/bin/n98-magerun2' do
+    source   "https://files.magerun.net/n98-magerun2-#{n98_version}.phar"
+    checksum n98_checksum
+    owner    n98_owner
+    group    n98_group
+    mode     '0755'
+    action   :create
+end
