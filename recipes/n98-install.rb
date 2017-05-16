5ce9373b57835a2accbdaaac1a56b2b60bf15c58
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-16 16:48:28 -0700

    Simplify attributes

--- recipes/n98-install.rb
+++ recipes/n98-install.rb
@@ -3,19 +3,14 @@
 # Recipe:: n98-install
 #
 
-n98_version  = node['magento']['n98']['version']
-n98_checksum = node['magento']['n98']['checksum']
-n98_path     = node['magento']['n98']['path']
-n98_owner    = node['magento']['n98']['owner']
-n98_group    = node['magento']['n98']['group'] 
-n98_mode     = node['magento']['n98']['mode'] 
+n98 = node['magento']['n98']
 
 remote_file 'N98-Magerun CLI Tool' do
-    source   "https://files.magerun.net/n98-magerun2-#{n98_version}.phar"
-    checksum n98_checksum
-    path     n98_path
-    owner    n98_owner
-    group    n98_group
-    mode     n98_mode
+    source   "https://files.magerun.net/n98-magerun2-#{n98['version']}.phar"
+    checksum n98['checksum']
+    path     n98['path']
+    owner    n98['owner']
+    group    n98['group']
+    mode     n98['mode']
     action   :create
 end
