3db0102b744f34936d605b957d1f1a4adee4c833
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-16 09:21:39 -0700

    Use new attributes

--- recipes/n98-install.rb
+++ recipes/n98-install.rb
@@ -5,15 +5,17 @@
 
 n98_version  = node['magento']['n98']['version']
 n98_checksum = node['magento']['n98']['checksum']
-n98_owner    = node['magento']['cli_user']['name']
-n98_group    = node['magento']['cli_user']['group'] 
+n98_path     = node['magento']['n98']['path']
+n98_owner    = node['magento']['n98']['owner']
+n98_group    = node['magento']['n98']['group'] 
+n98_mode     = node['magento']['n98']['mode'] 
 
 remote_file 'N98-Magerun CLI Tool' do
     source   "https://files.magerun.net/n98-magerun2-#{n98_version}.phar"
-    path     '/usr/local/bin/n98-magerun2'
     checksum n98_checksum
+    path     n98_path
     owner    n98_owner
     group    n98_group
-    mode     '0755'
+    mode     n98_mode
     action   :create
 end
