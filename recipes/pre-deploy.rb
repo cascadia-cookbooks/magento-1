0f4a38e48449abc6773ec3ec692071f52e303634
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-21 11:43:52 -0700

    Remove recipe from run lists

--- recipes/pre-deploy.rb
+++ recipes/pre-deploy.rb
@@ -6,7 +6,6 @@
 include_recipe 'cop_magento::data-bag'
 include_recipe 'cop_magento::n98-install'
 include_recipe 'cop_magento::dir-structure'
-include_recipe 'cop_magento::permissions'
 include_recipe 'cop_magento::robots'
 include_recipe 'cop_magento::composer-prep'
 include_recipe 'cop_magento::nginx-vhost'
