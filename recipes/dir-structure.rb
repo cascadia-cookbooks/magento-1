5e929c344abaa50cdff69fb49e85d06c136c831a
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-05-23 17:06:03 -0700

    Define vagrant_dirs and create if environment = development

--- recipes/dir-structure.rb
+++ recipes/dir-structure.rb
@@ -33,6 +33,17 @@ dirs = %w(
     shared/var
 )
 
+vagrant_dirs = %w(
+    /vagrant
+    /vagrant/magento
+    /vagrant/magento/app
+    /vagrant/magento/app/etc
+    /vagrant/magento/var
+    /vagrant/magento/var/session
+    /vagrant/magento/vendor
+)
+
+
 # Create structure for default domain
 directory docroot do
     owner     cli_user
@@ -66,6 +77,18 @@ additional_domains.each do |domain|
         directory "/var/www/#{domain}/#{dir}" do
             owner     cli_user
             group     www_group
+            mode      0775
+            recursive true
+            action    :create
+        end
+    end
+end
+
+if node.chef_environment == "development"
+    vagrant_dirs.each do |dir|
+        directory dir do
+            owner     'vagrant'
+            group     'vagrant'
             mode      0775
             recursive true
             action    :create
