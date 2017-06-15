917d5013de79c3a2149384a567a38d19854f945c
Hardy Johnson <hardyj@copiousinc.com>
2017-06-15 14:06:54 -0700

    Update default attributes, start separation of attributes into individual files.

---
+++ attributes/composer.rb
@@ -0,0 +1,23 @@
+default['magento']['composer'] = {
+    binary: {
+        path: '/usr/local/bin/composer'
+    },
+    description: 'eCommerce Platform for Growth (Enterprise Edition)',
+    merge: {
+        version: 'v1.3.1',
+        recurse: false,
+        replace: true,
+        merge_dev: true,
+        merge_extra: true
+    },
+    name: 'magento/copious-dev-box',
+    repositories: {
+        'repo.magento.com' => {
+            username: '',
+            password: ''
+        },
+        copious_repos: {
+            token: ''
+        }
+    }
+}
