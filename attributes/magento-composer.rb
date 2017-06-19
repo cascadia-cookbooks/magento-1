99a86ae462fd9c348a36915d9eb73ae226739672
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-06-19 15:04:11 -0700

    Rename/break out attributes

---
+++ attributes/magento-composer.rb
@@ -0,0 +1,20 @@
+default['magento']['composer'] = {
+    description: 'eCommerce Platform for Growth (Enterprise Edition)',
+    merge: {
+        version: 'v1.3.1',
+        recurse: false,
+        replace: true,
+        merge_dev: true,
+        merge_extra: true
+    },
+    name: 'magento/copious-dev-box',
+    repositories: {
+        'repo.magento.com' => {
+            username: '',
+            password: ''
+        },
+        copious_repos: {
+            token: ''
+        }
+    }
+}
--- attributes/magento-nginx.rb
+++ attributes/magento-nginx.rb
@@ -1,4 +1,4 @@
-# nginx magento.conf variables
+# NGINX magento.conf template variables
 default['magento']['nginx'] = {
     fastcgi_read_timeout: '60s',
     fastcgi_connect_timeout: '60s'
